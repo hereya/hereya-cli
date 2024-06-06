fe50bedb6e07209eefe65a1e90cacb1cdbd6ba26
Jonatan Sombié <jonatan.sombie@gmail.com>
2024-06-06 14:07:39 +0200

    fix: Add arg and flag handling for WorkspaceEnv command
    Updated the WorkspaceEnv command to accept, process, and utilize new arguments and flags. Specifically, the command now can handle a name argument for env var display, and a list flag for listing env vars without values. Padded out the run method in the WorkspaceEnv command to actually utilize these new arguments and flags. Also, fixed a minor indentation issue in the workspace delete command.
    
    Signed-off-by: Jonatan Sombié <jonatan.sombie@gmail.com>

--- src/commands/workspace/env/index.ts
+++ src/commands/workspace/env/index.ts
@@ -1,16 +1,27 @@
-import { Command, Flags } from '@oclif/core'
+import { Args, Command, Flags } from '@oclif/core'
 
 import { getBackend } from '../../../backend/index.js';
 import { logEnv } from '../../../lib/env-utils.js';
 
 export default class WorkspaceEnv extends Command {
+    static override args = {
+        name: Args.string({ description: 'name of the env to display', required: false }),
+    }
+
     static override description = 'Print workspace env vars.'
 
     static override examples = [
         '<%= config.bin %> <%= command.id %> -w dev',
+        '<%= config.bin %> <%= command.id %> myEnv -w dev',
     ]
 
     static override flags = {
+        list: Flags.boolean({
+            char: 'l',
+            description: 'list only the env vars without values',
+            required: false,
+        }),
+
         workspace: Flags.string({
             char: 'w',
             description: 'name of the workspace to print env vars for',
@@ -19,7 +30,7 @@ export default class WorkspaceEnv extends Command {
     }
 
     public async run(): Promise<void> {
-        const { flags } = await this.parse(WorkspaceEnv)
+        const { args, flags } = await this.parse(WorkspaceEnv)
 
         const backend = await getBackend()
         const getWorkspaceEnvOutput = await backend.getWorkspaceEnv({
@@ -30,6 +41,20 @@ export default class WorkspaceEnv extends Command {
         }
 
         const { env } = getWorkspaceEnvOutput
+        if (args.name) {
+            if (env[args.name] === undefined) {
+                this.error(`Env var ${args.name} not found`)
+            }
+
+            this.log(env[args.name])
+            return
+        }
+
+        if (flags.list) {
+            this.log(Object.keys(env).join('\n'))
+            return
+        }
+
         logEnv(env)
     }
 }
