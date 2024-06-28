06cda28aad1106d8fe9ac578c8dc5a8ad18ab69f
Jonatan Sombié <jonatan.sombie@gmail.com>
2024-06-28 14:30:19 +0200

    Add Terraform binary downloading support
    In the Terraform class, methods have been modified to use a downloaded Terraform binary instead of relying on a globally installed binary. Added a `downloadTerraform` method to handle the downloading of the appropriate binary based on the operating system and architecture. Also added the `unzip-stream` package and its types to handle decompression of the downloaded zip file.
    
    Signed-off-by: Jonatan Sombié <jonatan.sombie@gmail.com>

--- src/iac/terraform.ts
+++ src/iac/terraform.ts
@@ -1,3 +1,10 @@
+import fs from 'node:fs';
+import https from 'node:https';
+import os from 'node:os';
+import path from 'node:path';
+import { pipeline } from 'node:stream/promises';
+import { Extract as decompress } from 'unzip-stream'
+
 import { mapObject } from '../lib/object-utils.js';
 import { runShell } from '../lib/shell.js';
 import { ApplyInput, ApplyOutput, DestroyInput, DestroyOutput, Iac } from './common.js';
@@ -5,8 +12,10 @@ import { ApplyInput, ApplyOutput, DestroyInput, DestroyOutput, Iac } from './com
 export class Terraform implements Iac {
     async apply(input: ApplyInput): Promise<ApplyOutput> {
         try {
+            const terraform = await this.getTerraformBinary()
+
             runShell(
-                'terraform',
+                terraform,
                 ['init'],
                 {
                     directory: input.pkgPath,
@@ -17,7 +26,7 @@ export class Terraform implements Iac {
                 }
             )
             runShell(
-                'terraform',
+                terraform,
                 ['apply', '-auto-approve'],
                 {
                     directory: input.pkgPath,
@@ -42,9 +51,10 @@ export class Terraform implements Iac {
     }
 
     async destroy(input: DestroyInput): Promise<DestroyOutput> {
+        const terraform = await this.getTerraformBinary()
         try {
             runShell(
-                'terraform',
+                terraform,
                 ['init'],
                 {
                     directory: input.pkgPath,
@@ -56,7 +66,7 @@ export class Terraform implements Iac {
             )
             const env = await this.getEnv(input.pkgPath)
             runShell(
-                'terraform',
+                terraform,
                 ['destroy', '-auto-approve'],
                 {
                     directory: input.pkgPath,
@@ -78,9 +88,74 @@ export class Terraform implements Iac {
         }
     }
 
+    private async downloadTerraform(): Promise<boolean> {
+        const TERRAFORM_DOWNLOAD_URLS = new Map<string, string>([
+            ['darwin_arm64', 'https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_darwin_arm64.zip'],
+            ['darwin_x64', 'https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_darwin_amd64.zip'],
+            ['freebsd_arm', 'https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_freebsd_arm.zip'],
+            ['freebsd_ia32', 'https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_freebsd_386.zip'],
+            ['freebsd_x64', 'https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_freebsd_amd64.zip'],
+            ['linux_arm', 'https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_linux_arm.zip'],
+            ['linux_arm64', 'https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_linux_arm64.zip'],
+            ['linux_ia32', 'https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_linux_386.zip'],
+            ['linux_x64', 'https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_linux_amd64.zip'],
+            ['openbsd_ia32', 'https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_openbsd_386.zip'],
+            ['openbsd_x64', 'https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_openbsd_amd64.zip'],
+            ['sunos_x64', 'https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_solaris_amd64.zip'],
+            ['win32_ia32', 'https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_windows_386.zip'],
+            ['win32_x64', 'https://releases.hashicorp.com/terraform/1.5.7/terraform_1.5.7_windows_amd64.zip'],
+        ])
+
+        const tfPath = this.terraformBinPath()
+
+        try {
+            await fs.promises.access(tfPath, fs.constants.X_OK)
+            return false
+        } catch (error: any) {
+            // file does not exist or is corrupted - continue
+            if (error.code !== 'ENOENT') {
+                await fs.promises.rm(tfPath)
+            }
+        }
+
+        console.log('Downloading terraform...')
+
+        const binKey = `${os.platform()}_${os.arch()}`
+        const url = TERRAFORM_DOWNLOAD_URLS.get(binKey)
+
+        if (!url) {
+            throw new Error('unsupported platform for terraform')
+        }
+
+        try {
+            await fs.promises.mkdir(path.dirname(tfPath), { recursive: true })
+        } catch (error: any) {
+            if (error.code !== 'EEXIST') {
+                throw new Error(`could not download terraform: ${error}`)
+            }
+        }
+
+        await new Promise((resolve, reject) => {
+            https.get(url, async response => {
+                try {
+                    await pipeline(response, decompress({ path: path.dirname(tfPath) }))
+                    await fs.promises.chmod(tfPath, '0755')
+                } catch (error) {
+                    throw new Error(`could not download terraform: ${error}`)
+                }
+
+                resolve(null)
+            }).on('error', error => reject(error))
+        })
+
+        return true
+
+    }
+
     private async getEnv(pkgPath: string): Promise<{ [k: string]: string }> {
+        const terraform = await this.getTerraformBinary()
         const resourceOut = runShell(
-            'terraform',
+            terraform,
             ['output', '--json'],
             {
                 directory: pkgPath,
@@ -103,6 +178,15 @@ export class Terraform implements Iac {
         }
 
         return tfEnv
+    }
+
+    private async getTerraformBinary(): Promise<string> {
+        await this.downloadTerraform()
+        return this.terraformBinPath()
+    }
+
+    private terraformBinPath() {
+        return path.join(os.homedir(), '.hereya', 'iac', 'terraform', os.platform() === 'win32' ? 'terraform.exe' : 'terraform')
     }
 
 }
